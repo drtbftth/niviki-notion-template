c0715737d97cf0c73c9ecfcc2601ba9105c35e6f
drtbftth <121483737+drtbftth@users.noreply.github.com>
2023-04-05 10:35:42 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,28 +3,28 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Medical-Lectures-96831b6b2e7d49c49eb85bc4af46ee62',
+  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Medical Lectures',
-  domain: 'tbftth.online',
-  author: 'TBFTTH',
+  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
+  domain: 'niviki.com',
+  author: 'Khoa Nguyen',
 
   // open graph metadata (optional)
-  description: 'Học Tốt - Mơ Nhiều - Yêu Say Đắm',
+  description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
 
   // social usernames (optional)
-  twitter: 'tbftth1',
+  twitter: 'khoa_nvk',
   // github: 'transitive-bullshit',
-  linkedin: 'tbftth',
+  linkedin: 'khoanguyen08',
   
-  facebook: 'https://www.facebook.com/tbftth',
+  facebook: 'https://www.facebook.com/groups/nocodevietnam',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
-  youtube: 'channel/UC2hASZDjUaupqGYli4a0XkA', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -58,19 +58,19 @@ export default siteConfig({
   navigationLinks: [
     {
       title: 'Blog',
-      pageId: '93db1fa3d2144643ab9241b3620a0e75'
+      pageId: '59432e1a0cdf4effbb402701deff00a9'
     },    
     {
-      title: 'Contact',
-      pageId: '96b08a422ba747f8903ce1df6354e271'
+      title: 'NoCode VN',
+      pageId: '93331fa2964045248502bd314019c917'
     },    
     {
-      title: 'Medical Books',
-      pageId: '512a4397112d4cf6962cfcda9e388c29'
+      title: 'Khoá học',
+      pageId: '8e761b9e7d1b4db1ae76bbc1e4f9aa1f'
     },    
     {
       title: 'About',
-      pageId: 'ac24260f99954ad584c1f72848ea60a6'
+      pageId: '38979ee661fc4a5d8af2d7c32319ee0d'
     }
   ]
 })
