7b4fd120ab830ff70a8caee5eb87f62d631c6fc9
drtbftth <121483737+drtbftth@users.noreply.github.com>
2023-04-05 10:42:16 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,25 +3,25 @@ import { siteConfig } from './lib/site-config'
 export default siteConfig({
   // the site's root Notion page (required)
   // rootNotionPageId: 'niviki-com-t-do-t-i-ch-nh-10f5e54221eb4b059bc5b5305da949ba',
-  rootNotionPageId: 'Simple-Template-49955b133eb843bdb8665534ecfd844c',
+  rootNotionPageId: 'Medical-Lectures-767866161eab456b9d5d600d8040b526',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'NIVIKI.COM - TỰ DO TÀI CHÍNH',
-  domain: 'niviki.com',
-  author: 'Khoa Nguyen',
+  name: 'Medical Lectures',
+  domain: 'tbftth.click',
+  author: 'TBFTTH',
 
   // open graph metadata (optional)
-  description: 'Lan toả tinh thần tự do tài chính đến với nhiều người hơn nữa! Tự do tài chính là bạn được quyền “thất nghiệp” nhưng vẫn có đủ tiền cho một cuộc sống thoải mái trong thời gian dài',
+  description: 'Học Tốt - Mơ Nhiều - Yêu Say Đắm',
 
   // social usernames (optional)
-  twitter: 'khoa_nvk',
+  twitter: 'tbftth1',
   // github: 'transitive-bullshit',
-  linkedin: 'khoanguyen08',
+  linkedin: 'tbftth',
   
-  facebook: 'https://www.facebook.com/groups/nocodevietnam',
+  facebook: 'https://www.facebook.com/tbftth',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   youtube: 'channel/UCI6GDuK41kD9T8icFp8d7Aw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
