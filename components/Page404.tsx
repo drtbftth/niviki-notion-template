03ed83446a8370c0e36611dbf7d9b802e1588e72
drtbftth <121483737+drtbftth@users.noreply.github.com>
2023-04-05 11:37:59 +0700

    Update Page404.tsx

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -6,7 +6,7 @@ import { PageHead } from './PageHead'
 import styles from './styles.module.css'
 
 export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
-  const title = site?.name || 'Thật Không May Bài Viết Bạn Vừa Click Vào Chưa Có Sẵn, Chờ xíu nha để tui fix'
+  const title = site?.name || 'Lỗi rồi nè'
 
   return (
     <>
@@ -15,8 +15,8 @@ export const Page404: React.FC<types.PageProps> = ({ site, pageId, error }) => {
 
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1>Page Not Found</h1>
-          <a href='/'>Home</a> <a href='/sitemap.xml'>SiteMap</a>
+          <h1>Thật Không May Bài Viết Bạn Vừa Click Vào Chưa Có Sẵn, Chờ xíu nha để tui fix</h1>
+          <a href='/'>Về trang chủ nhá</a> <a href='/sitemap.xml'>SiteMap</a>
           {error ? (
             <p>{error.message}</p>
           ) : (
